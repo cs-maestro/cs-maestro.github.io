65edb1970a85d8c0bfb45c26ba56b30e775a5d64
cs-maestro <79442514+cs-maestro@users.noreply.github.com>
2025-09-24 04:57:03 +0000

    Deploying to gh-pages from @ cs-maestro/cs-maestro.github.io@d85b2e2bccb20b5ecc7fb173ec896a450ea93960 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "cs-maestro/mdanish.net",
+    "data-repo": "cs-maestro/cs-maestro.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",

3c8c813798a3f87ad87c443bc3ec1df1340248be
cs-maestro <79442514+cs-maestro@users.noreply.github.com>
2025-12-30 19:18:36 +0000

    Deploying to gh-pages from @ cs-maestro/cs-maestro.github.io@011cade57f79b727f96c89eeab4a697331fe1bd6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -98,13 +98,6 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=T1J6algAAAAJ", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
